128252c3e7048b1bf62df8fb9bb4c509f29ffba7
AndresMate <andres.mateus01@uptc.edu.co>
2024-11-28 17:59:00 -0500

    mi parcial

---
+++ src/test/java/edu/uptc/parcialspring/ParcialspringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.parcialspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
